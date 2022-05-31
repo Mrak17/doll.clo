11f4bc34480363237aeee556bb4b548c937b33a8
sasha <sashokklas17112002@gmail.com>
2022-05-31 09:59:16 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-30">
+        <div className="logo d-flex">
+          <img height={60} src="/img/logo_text.svg" alt="Logotype" />
+          <p className="opacity-7">Магазин лялькового одягу</p>
+        </div>
+        <ul className="d-flex">
+          <li>Головна</li>
+          <li>Про мене</li>
+          <li>Товари</li>
+          <li>Відгуки</li>
+          <li>Контакти</li>
+        </ul>
       </header>
+      <div className="content p-30">
+        <h2>Про мене</h2>
+        <p>Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів. "Риба" не тільки успішно пережила п'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною. Вона популяризувалась в 60-их роках минулого сторіччя завдяки виданню зразків шрифтів Letraset, які містили уривки з Lorem Ipsum, і вдруге - нещодавно завдяки програмам комп'ютерного верстування на кшталт Aldus Pagemaker, які використовували різні версії Lorem Ipsum.</p>
+      </div>
     </div>
   );
 }
