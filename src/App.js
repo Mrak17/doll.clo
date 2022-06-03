47cf9a5062c564813a44fd9c9b291770955dcf21
sasha <sashokklas17112002@gmail.com>
2022-06-03 12:52:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -8,21 +8,50 @@ function App() {
             <p className="opacity-7">Магазин лялькового одягу</p>
           </div>
         </a>
-        <ul className="d-flex">
-          <li><a href="#Home">Головна</a></li>
+        <ul className="d-flex mr-40">
           <li><a href="#About">Про мене</a></li>
-          <li>Товари</li>
+          <li><a href="#Ware">Товари</a></li>
           <li>Відгуки</li>
           <li>Контакти</li>
         </ul>
       </header>
       <div className="content p-30">
-        <div className="about d-flex" id="About">
+        <div className="about d-flex mb-40" id="About">
           <div className="d-flex flex-column justify-center">
             <h2>Про мене</h2>
-            <p>Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів. "Риба" не тільки успішно пережила п'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною. Вона популяризувалась в 60-их роках минулого сторіччя завдяки виданню зразків шрифтів Letraset, які містили уривки з Lorem Ipsum, і вдруге - нещодавно завдяки програмам комп'ютерного верстування на кшталт Aldus Pagemaker, які використовували різні версії Lorem Ipsum.</p>
+            <p>Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів. "Риба" не тільки успішно пережила п'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною.</p>
           </div>
           <img height={700} src="/img/about.png" alt="" />
+        </div>
+        <hr></hr>
+        <div className="ware mt-40" id="Ware">
+          <h2>Мої товари</h2>
+          <div className="cards d-flex">
+            <div className="card d-flex flex-column">
+              <img width={250} src="/img/ware/1.jpg" alt=""></img>
+              <p className="opacity-7 fw-bold">Кофта з люрексу</p>
+            </div>
+            <div className="card d-flex flex-column ">
+              <img width={250} src="/img/ware/2.jpg" alt=""></img>
+              <p className="opacity-7 fw-bold">Кофта з люрексу</p>
+            </div>
+            <div className="card d-flex flex-column ">
+              <img width={250} src="/img/ware/3.jpg" alt=""></img>
+              <p className="opacity-7 fw-bold">Кофта з люрексу</p>
+            </div>
+            <div className="card d-flex flex-column ">
+              <img width={250} src="/img/ware/4.jpg" alt=""></img>
+              <p className="opacity-7 fw-bold">Кофта з люрексу</p>
+            </div>
+            <div className="card d-flex flex-column ">
+              <img width={250} src="/img/ware/5.jpg" alt=""></img>
+              <p className="opacity-7 fw-bold">Кофта з люрексу</p>
+            </div>
+            <div className="card d-flex flex-column ">
+              <img width={250} src="/img/ware/6.jpg" alt=""></img>
+              <p className="opacity-7 fw-bold">Кофта з люрексу</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
